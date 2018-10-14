39b06a544578e17ef4424a33d32c39f72661eda9
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-13 21:47:20 -0700

    more files

---
+++ build/precache-manifest.25f7b3a1d7232baf205dd84f11aac235.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "53f9945e738b7efae3fa",
+    "url": "/ny-times/static/js/runtime~main.53f9945e.js"
+  },
+  {
+    "revision": "4a9e356e481e7d6756d1",
+    "url": "/ny-times/static/js/main.4a9e356e.chunk.js"
+  },
+  {
+    "revision": "aba28e1908470ab59ef3",
+    "url": "/ny-times/static/js/1.aba28e19.chunk.js"
+  },
+  {
+    "revision": "e4d19cfd8ed3060e833a09e55519b98e",
+    "url": "/ny-times/index.html"
+  }
+];
